c976442db05bf70ee889df02a3a9546ed56cafce
Lake Yang <lake.yang@kupotech.com>
2026-05-17 16:45:58 +0800

    pddrefund demo

--- function-call/src/main/java/com/example/functioncall/pddrefund/tools/OrderTools.java
+++ function-call/src/main/java/com/example/functioncall/pddrefund/tools/OrderTools.java
@@ -16,6 +16,7 @@ public class OrderTools {
     public String refund(@ToolParam(description = "订单编号，为数字类型") String orderId, @ToolParam(description = "商品名称") String name, @ToolParam(description = "退款原因") String reason) {
         System.out.println("已为商品:" + name + ",订单号:" + orderId + "申请退款 , 退款原因： " + reason);
 
+        // 调用订单系统的退款接口
         orderManageService.refund(orderId, reason);
 
         return "已为商品：" + name + ",订单号：" + orderId + "申请退款 , 退款原因： " + reason;
