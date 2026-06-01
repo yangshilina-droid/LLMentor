33055c1443e11404645dd08e16253d7d9d318a1d
line <yangshiline@163.com>
2026-06-02 00:07:57 +0800

    mcp stdio

---
+++ mcp/mcp-server-sse/src/main/java/com/lake/mcpserversse/McpServerSseApplication.java
@@ -0,0 +1,13 @@
+package com.lake.mcpserversse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpServerSseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpServerSseApplication.class, args);
+    }
+
+}
---
+++ mcp/mcp-server-sse/src/test/java/com/lake/mcpserversse/McpServerSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lake.mcpserversse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpServerSseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
