414e2114713f6d43ff4333498f4fbcf91c17e704
line <yangshiline@163.com>
2026-06-08 23:07:58 +0800

    dodo-agent初始化

---
+++ agent/dodo-agent/src/main/java/com/lake/dodoagent/DodoAgentApplication.java
@@ -0,0 +1,13 @@
+package com.lake.dodoagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodoAgentApplication.class, args);
+    }
+
+}
---
+++ agent/dodo-agent/src/test/java/com/lake/dodoagent/DodoAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lake.dodoagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodoAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
