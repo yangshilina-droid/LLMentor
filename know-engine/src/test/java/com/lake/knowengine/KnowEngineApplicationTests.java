945cdf0d78f56b6439c602d0f020444174d59add
line <yangshiline@163.com>
2026-06-30 00:07:55 +0800

    know-engine项目初始化

---
+++ know-engine/src/test/java/com/lake/knowengine/KnowEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lake.knowengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
