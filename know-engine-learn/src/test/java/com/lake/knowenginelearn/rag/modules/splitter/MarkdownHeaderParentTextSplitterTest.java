08fc344a5a3834391b65192b973df00634147991
line <yangshiline@163.com>
2026-09-05 22:47:50 +0800

    bugfix

--- know-engine-learn/src/test/java/com/lake/knowenginelearn/rag/modules/splitter/MarkdownHeaderParentTextSplitterTest.java
+++ know-engine-learn/src/test/java/com/lake/knowenginelearn/rag/modules/splitter/MarkdownHeaderParentTextSplitterTest.java
@@ -4,27 +4,24 @@ import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.DocumentParser;
 import dev.langchain4j.data.document.parser.TextDocumentParser;
 import dev.langchain4j.data.segment.TextSegment;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import java.io.InputStream;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author LAKE.YANG
  * @filename MarkdownHeaderParentTextSplitterTest
  * @date 2026-07-08 00:18
  */
-class MarkdownHeaderParentTextSplitterTest {
+public class MarkdownHeaderParentTextSplitterTest {
 
     @Test
-    void split() {
+    public void testSplitText() {
 
         MarkdownHeaderParentTextSplitter markdownHeaderParentTextSplitter = new MarkdownHeaderParentTextSplitter(3, false, false, 1000, 100);
-        // MinerU_markdown_r7-product-manual-20250123_2028781865782407168.md
-        // MinerU_markdown_r7-product-manual-for-testset.processed.md
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("MinerU_markdown_r7-product-manual-20250123_2028781865782407168.md");
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("MinerU_markdown_r7-product-manual-for-testset.processed.md");
         DocumentParser parser = new TextDocumentParser();
         Document parsedDocument = parser.parse(inputStream);
         List<TextSegment> segments = markdownHeaderParentTextSplitter.split(parsedDocument);
